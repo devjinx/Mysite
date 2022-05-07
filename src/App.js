7bc229d4193e2684375cc1d78f03f2cd91fb974b
devjinx <jeongseokjin28@gmail.com>
2022-05-07 17:53:42 +0700

    firstpath

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App-header">
+       <font size ="10"><p1>devjinx</p1></font>
+       <font size ="5"><p2>Chareonlertkamol Thanakorns</p2></font>
+       <font size="5"><p3>Student Software Develope Freelance-Dev</p3></font>
+       <h1> </h1>
+       <a href="https://github.com/devjinx">Github</a>
+       <h1> </h1>
+       <a href="https://linktr.ee/devjinx">Resume/Certificate</a>
+    </div>
+  );
+}
+
+export default App;
